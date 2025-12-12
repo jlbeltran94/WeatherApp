653328bd612c46f651a06e8705692ca5d3d293f2
Jose Beltran <jlbeltran94@gmail.com>
2025-12-12 01:15:13 -0500

    refactor: remove compose and other UI dependencies from commonNetwork

--- commonNetwork/build.gradle.kts
+++ commonNetwork/build.gradle.kts
@@ -3,7 +3,6 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.detekt)
@@ -42,7 +41,6 @@ android {
         }
     }
     buildFeatures {
-        compose = true
         buildConfig = true
     }
     compileOptions {
@@ -56,46 +54,15 @@ android {
 
 dependencies {
     implementation(project(":commonModel"))
-    // Core Android
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-
-    // Compose
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-
     // Hilt
     implementation(libs.hilt.android)
-    implementation(libs.androidx.junit.ktx)
     ksp(libs.hilt.compiler)
-    implementation(libs.hilt.navigation.compose)
 
     // Retrofit & OkHttp
     implementation(libs.retrofit)
     implementation(libs.retrofit.kotlinx.serialization)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
-
-    // Room
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
-    ksp(libs.room.compiler)
-
-    // Lottie
-    implementation(libs.lottie.compose)
-
-    // Navigation
-    implementation(libs.navigation.compose)
-
-    // ViewModel
-    implementation(libs.lifecycle.viewmodel.compose)
-
-    // Coil
-    implementation(libs.coil.compose)
 
     // Kotlinx Serialization
     implementation(libs.kotlinx.serialization.json)
@@ -114,18 +81,12 @@ dependencies {
     testImplementation(libs.androidx.core.testing)
     testImplementation(libs.hilt.testing)
     testImplementation(libs.robolectric)
-    testImplementation(libs.androidx.compose.ui.test.junit4)
     kspTest(libs.hilt.compiler)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.hilt.testing)
     androidTestImplementation(libs.mockk.android)
     androidTestImplementation(libs.androidx.core.testing)
     kspAndroidTest(libs.hilt.compiler)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
